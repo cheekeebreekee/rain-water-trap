c1168acc2556c676f2890554741faaecde0371d2
Yahor Simak <simak_yahor@mail.ru>
2018-01-10 23:57:08 +0300

    all test passed in trap-rain-water task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function trapRainWater(elevationMap) {
-    // your solution here
+    let output = 0, leftBorder = 0, rightBorder = elevationMap.length - 1,
+        maxLeftBorder = 0, maxRightBorder = 0;
+    
+    while (leftBorder <= rightBorder) {
+        if (elevationMap[leftBorder] <= elevationMap[rightBorder]) {
+            if (elevationMap[leftBorder] >=maxLeftBorder) {
+                maxLeftBorder = elevationMap[leftBorder];
+            }
+            else {
+                output += maxLeftBorder-elevationMap[leftBorder];
+            }
+            leftBorder++;
+        }
+        else {
+            if (elevationMap[rightBorder] >= maxRightBorder) {
+                maxRightBorder = elevationMap[rightBorder];
+            }
+            else {
+                output += maxRightBorder-elevationMap[rightBorder];
+            }
+            rightBorder--;
+        }        
+    }
+    return output;
 }
